c4440b3ba9841cb5ecf02c61fe0a273a69822622
ecuber <37519236+ecuber@users.noreply.github.com>
2021-08-09 21:35:11 -0400

    Add save function prop to demo

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# Math3d-React [![Build Status](https://travis-ci.com/ChristopherChudzicki/math3d-react.svg?branch=master)](https://travis-ci.com/ChristopherChudzicki/math3d-react)
-
-Live at [math3d.org](https://www.math3d.org)
+# Math3D React Component
 
 ## Package Anatomy
 
@@ -10,54 +8,10 @@ This repository has three `package.json` files:
 - `math3d-react/client/package.json` client dependencies and scripts
 - `math3d-react/package.json` deployment & development scripts
 
+## About
 
-## To Install for Local Development:
+The files in this repository have been forked from the [math3d-react](https://github.com/ChristopherChudzicki/math3d-react) website repository. Some modifications have been made from the original project to adapt it to function independently as its own component.
 
-1. **Install Postgresql:** If it is not already installed, you'll need to install `postgres` as our database. On a Mac, we recommend installing `postgres` with Homebrew:
+## Using in your own project
 
-    ```bash
-    > brew update
-    > brew install postgresql
-    ```
-
-1. **Bootstrapping the database:** Create a database cluster and start Postgres 
-
-    ```bash
-    # creates a new database cluster
-    > initdb /usr/local/var/postgres
-    # starts postgres
-    > pg_ctl -D /usr/local/var/postgres start
-    # create user math3d_user and database math3d
-    > psql -d postgres -f server/migrations/create_database.sql
-    # create schema
-    > psql -U math3d_user -d math3d -f server/migrations/database_setup.sql
-    ```
-
-1. **Set Database Connection:** Create a `.env` file in the `server/` directory to set `DATABASE_URL` database connection environment variable. For local development, just copy the template:
-
-    ```bash
-    > cp server/dotenv_template server/.env
-    ```
-
-1. **Install Dependencies:** Clone the git repo and `cd` to package root, then run:
-
-    ```bash
-    > npm install
-    ```
-
-    which installs both client and server dependencies.
-
-
-1. **Start Server & Client:** In a new terminal window, start the server:
-
-    ```bash
-    > npm run start:dev:server
-    ```
-
-    and, in a third terminal window, start the client app:
-
-    ```bash
-    > npm run start:dev:client
-    ```
-
-The math3d-react app is now being served on `http://localhost:3000/`.
+The component itself is published on [npm](https://npmjs.org) as [math3d-component](https://www.npmjs.com/package/math3d-component). 
--- app/src/App.js
+++ app/src/App.js
@@ -14,13 +14,28 @@ const Container = styled.div`
 `
 const graphStyles = { width: 744, boxShadow: 'lightgray 0 0 30px 2px'}
 
+const saveGraph = (dehydrated) => {
+  fetch('dev/save', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ dehydrated })
+} )
+.then((res) => {
+  if (res.ok) {
+    console.log('successfully saved graph!')
+  }
+} )
+}
+
 function App() {
   return<>
   <h1>hello world this is a header</h1>
   <p>this is a paragraph</p>
   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ultricies, sapien eu feugiat ullamcorper, enim nunc posuere ligula, nec maximus risus nulla nec orci. Nullam sem massa, congue sed ultricies eget, ultrices eget nunc. Aenean nec eros bibendum nisl fermentum tempor quis eu tortor. Nulla et nisl enim. Nullam dui tellus, auctor eget feugiat sed, volutpat eu ipsum. Donec lacinia, odio vitae mattis vestibulum, ex purus pellentesque ipsum, mollis sagittis velit arcu ullamcorper augue. In hac habitasse platea dictumst. Phasellus faucibus efficitur magna nec cursus. In aliquam mauris ac quam feugiat, sed dignissim nunc lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin sed massa et turpis tempus rhoncus. In ac mauris diam.</p>
   <Container>
-    <Math3D style={graphStyles} dehydrated={graphs.z6rPenvx} drawer={false}/>
+    <Math3D style={graphStyles} dehydrated={graphs.z6rPenvx}/>
   </Container>
   <p>
 Pellentesque nisi odio, consequat eu dapibus eget, fermentum at dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque dolor lectus, eleifend imperdiet velit sodales, posuere euismod ligula. Curabitur varius, nibh sollicitudin dictum commodo, libero massa interdum arcu, et fermentum libero libero sed elit. Fusce non nisl libero. Mauris felis felis, viverra a consectetur at, hendrerit sit amet erat. Vestibulum non lacus lorem. Nunc vehicula turpis et accumsan venenatis. Vivamus vel augue non eros pellentesque vulputate non scelerisque augue. Cras tincidunt justo tincidunt ex vestibulum, id commodo augue tincidunt. Aenean pulvinar vulputate libero, ac rhoncus tellus tincidunt vitae. Proin porttitor tellus nisi, vitae suscipit erat iaculis sed. Morbi bibendum, est eget dapibus consectetur, lacus sapien condimentum lorem, vitae tempor risus nulla sit amet augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id mi bibendum, venenatis mauris sit amet, sodales lectus.
@@ -28,7 +43,7 @@ Pellentesque nisi odio, consequat eu dapibus eget, fermentum at dolor. Class apt
 Aenean sagittis sem eu nulla malesuada, sed condimentum felis pulvinar. Morbi a augue non ex venenatis dapibus vel nec nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam vel nibh libero. Morbi et massa eget enim viverra suscipit. Integer egestas porta faucibus. Proin vehicula, tortor eu convallis sollicitudin, turpis nibh luctus ligula, at scelerisque odio sapien hendrerit lacus. Sed ut fermentum turpis. Cras sit amet nisi magna. Duis pretium ante quis ornare tempus. Etiam ac tincidunt arcu. Mauris nec ligula enim. Cras consequat nisi id elementum efficitur. Fusce volutpat dapibus vulputate.
 </p>
   <Container>
-    <Math3D style={graphStyles} drawer={false}/>
+    <Math3D style={graphStyles} dehydrated={graphs.tZqhYXdi} save={saveGraph} dev/>
   </Container>
   </>
 }
