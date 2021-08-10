e99abf30cfb75b910a929dc65ecbc0439fbc6430
ecuber <37519236+ecuber@users.noreply.github.com>
2021-08-10 15:35:51 -0400

    Testing with astro
    v1.4.31

--- client/index.js
+++ client/index.js
@@ -1,4 +1,4 @@
-const Math3D = require('math3d-component/dist/bundle').default
+const Math3D = require('./dist/bundle').default
 
 module.exports = (props) => Math3D(props)
 module.exports.default = (props) => Math3D(props)
